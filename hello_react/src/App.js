cc767ba1c048dfa59344b500eb1dd0a16e69e217
WhiteB <whiteblock0715@gmail.com>
2020-11-03 15:35:24 +0900

    serve setting

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -4,12 +4,16 @@ import TOC from './components/TOC'
 import ReadContent from './components/ReadContent'
 import Subject from './components/Subject'
 import Control from './components/Control' 
+import CreateContent from './components/CreateContent'
+import UpdateContent from './components/UpdateContent'
 
 class App extends Component {
    constructor(props) {
-     super(props);
+
+     super(props)
+     this.max_content_id = 3;
      this.state = {
-       mode:'read',
+       mode:'welcome', 
        selected_content_id:2,
        welcome:{title:'Welcome', desc:"Hello, React!!"},
        subject:{title:'WEB', sub:"World Wide Web!"},
@@ -22,23 +26,91 @@ class App extends Component {
      }
    }
 
+getReadContent() {
 
-  render() {
-    var _title, _desc = null;
+  for(var data of this.state.contents) {
+    if(data.id === this.state.selected_content_id) {
+      return data;
+    }
+  }
+}
+
+
+  getContent() {
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome') {
       _title= this.state.welcome.title;
       _desc= this.state.welcome.desc;
-
+      _article=  <ReadContent title={_title} desc={_desc} ></ReadContent>
     }else if(this.state.mode === 'read') {
-      for(var i = 0; i<this.state.contents.length; i++) {
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
+
+        var _content = this.getReadContent();
+    
+
+        _article = <ReadContent title={_content.title} desc={_content.desc} ></ReadContent>
+
+
+      // for(var i = 0; i<this.state.contents.length; i++) {
+      //   var data = this.state.contents[i];
+      //   if(data.id === this.state.selected_content_id) {
          
-       _title= data.title;
-       _desc = data.desc;
+      //  _title= data.title;
+      //  _desc = data.desc;
+      //   }
+      // }
+    }else if(this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title,_desc) {
+
+        this.max_content_id += 1;
+
+        // this.state.contets.push({id:this.max_content_id, title:_title, desc:_desc});
+        // this.setState(this.state.contents);
+        //shouldComponent 메소드를 사용할 수 없음. newProps의 값과 이전 Props의 값이 같음.
+        //원본 수정X 복제본 수정O
+
+      //   var _contents = this.state.contents.concat({id:this.max_content_id, title:_title, desc:_desc});
+      //  this.setState({contents:_contents})
+
+       var newContents = Array.from(this.state.contents)
+       //객체일때 Object.assign(객체)로 하면 됨..
+      newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+      this.setState({contents: newContents})
+
+        //add Content to this.state.contents
+      }.bind(this)}></CreateContent>
+    }else if(this.state.mode === 'update') {
+      _content = this.getReadContent();
+      _article = <UpdateContent  data={_content} onSubmit={function(_id,_title,_desc) {
+        // this.state.contets.push({id:_id, title:_title, desc:_desc});
+        // this.setState(this.state.contents);
+        //shouldComponent 메소드를 사용할 수 없음. newProps의 값과 이전 Props의 값이 같음.
+        //원본 수정X 복제본 수정O
+
+      //   var _contents = this.state.contents.concat({id:_id, title:_title, desc:_desc});
+      //  this.setState({contents:_contents})
+
+       var UpdateContents = Array.from(this.state.contents)
+       //객체일때 Object.assign(객체)로 하면 됨..
+        var UpdateObject = UpdateContents.find((element) => {
+        if(element.id === _id) {
+          return true;
         }
-      }
+      });
+UpdateObject.title = _title;
+UpdateObject.desc = _desc;
+
+      
+
+      this.setState({contents: UpdateContents})
+
+        //add Content to this.state.contents
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+
+  }
+
+  render() {
     return (
       <div className="App">
        <Subject 
@@ -68,11 +140,28 @@ class App extends Component {
           })
         }.bind(this)}></TOC>
        <Control onChangeMode={function(_mode) {
-         this.setState({
-           mode: _mode
-         })
+         if(_mode === 'delete') {
+          if(window.confirm('continue?')) {
+
+            var _contents = Array.from(this.state.contents)
+
+            for(var i = 0; i<_contents.length; i++) {
+              if(_contents[i].id === this.state.selected_content_id) {
+               _contents.splice(i, 1);
+              }
+            }
+            this.setState({contents:_contents, mode:'welcome'})
+          }
+         }else {
+          this.setState({
+            mode: _mode  
+          })
+         }
+
+
+         
        }.bind(this)}></Control>
-        <ReadContent title={_title} desc={_desc} ></ReadContent>
+       {this.getContent()}
       </div>
     )
   }
