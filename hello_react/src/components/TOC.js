daa92a76ac18688032722e6a97f88cb191f450e9
WhiteB <whiteblock0715@gmail.com>
2020-11-03 15:36:00 +0900

    Create 마저 구현, update, delete 구현, 수강 끝.

--- hello_react/src/components/TOC.js
+++ hello_react/src/components/TOC.js
@@ -2,6 +2,21 @@ import React, { Component} from 'react';
 
 
 class TOC extends Component{
+    
+
+
+    // shouldComponentUpdate(newProps, newState) {
+      
+    //   console.log(newProps.data, this.props.data);
+
+    //   if(this.props.data === newProps.data) {
+    //     return false;
+    //   }else {
+    //     return true;
+    //   }
+    // }
+
+
     render() {
       var lists =[];
       var data = this.props.data;
