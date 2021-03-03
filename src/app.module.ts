5691bc292908f8c8c41fb8a27118d72c63e5ddce
WhiteB <whiteblock0715@gmail.com>
2021-03-03 10:31:03 +0900

    ect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 import { AdminController, TestController } from './test.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController, TestController, AdminController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
